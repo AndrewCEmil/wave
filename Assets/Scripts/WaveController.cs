f1ed89331067eaabe5bc4dba211faef9152dbaec
ace <andrew.emil@gmail.com>
2017-05-28 11:22:41 -0400

    Support for creating a mesh programatically

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -3,46 +3,47 @@ using System.Collections;
 
 public class WaveController : MonoBehaviour {
 
-	private Terrain terrain;
-	private TerrainData terrainData;
-	private bool hasGenerated;
-	private float hmWidth;
-	private float hmHeight;
-	private float mWidth;
-	private float mHeight;
 	// Use this for initialization
 	void Start () {
-		terrain = gameObject.GetComponent<Terrain>();
-		terrainData = terrain.terrainData;
-		hasGenerated = false;
-		hmWidth = terrainData.heightmapWidth;
-		hmHeight = terrainData.heightmapHeight;
-		mWidth = terrainData.size.x;
-		mHeight = terrainData.size.z;
 	}
 
 	void Update() {
-		float seed = Random.insideUnitCircle.x;
-		if (!hasGenerated) {
-
-			float[,] heights = terrainData.GetHeights (0, 0, (int)hmWidth, (int)hmHeight);
-			float current = 0f;
-			float max = 0f;
-			for (float i = 0; i < hmWidth; i++) {
-				for (float j = 0; j < hmHeight; j++) {
-					current = Mathf.PerlinNoise (seed + i / hmWidth, seed + j / hmHeight);
-					heights [(int)i, (int)j] = current;
-					if (current > max) {
-						max = current;
-					}
-				}
-			}
-
-			for (float i = 0; i < hmWidth; i++) {
-				heights [0, (int)i] = max + .1f;
-			}
-			terrainData.SetHeights (0, 0, heights);
-			hasGenerated = true;
-		}
 	}
+
+	void Awake() {
+		GameObject plane = new GameObject("Plane");
+		MeshFilter meshFilter = (MeshFilter)plane.AddComponent(typeof(MeshFilter));
+		meshFilter.mesh = CreateMesh(1, 0.2f);
+		MeshRenderer renderer = plane.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		renderer.material.shader = Shader.Find ("Particles/Additive");
+		Texture2D tex = new Texture2D(1, 1);
+		tex.SetPixel(0, 0, Color.green);
+		tex.Apply();
+		renderer.material.mainTexture = tex;
+		renderer.material.color = Color.green;
+	}
+
+	Mesh CreateMesh(float width, float height)
+	{
+		Mesh m = new Mesh();
+		m.name = "ScriptedMesh";
+		m.vertices = new Vector3[] {
+			new Vector3(-width, -height, 0.01f),
+			new Vector3(width, -height, 0.01f),
+			new Vector3(width, height, 0.01f),
+			new Vector3(-width, height, 0.01f)
+		};
+		m.uv = new Vector2[] {
+			new Vector2 (0, 0),
+			new Vector2 (0, 1),
+			new Vector2(1, 1),
+			new Vector2 (1, 0)
+		};
+		m.triangles = new int[] { 0, 1, 2, 0, 2, 3};
+		m.RecalculateNormals();
+
+		return m;
+	}
+
+
 }
